6ef67cee303417438164f756c22eab4d4a8bf429
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-01-22 01:13:08 -0200

    JPA, Hibernate e Flyway

--- src/main/java/com/algaworks/brewer/model/Estilo.java
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -1,12 +1,39 @@
 package com.algaworks.brewer.model;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-public class Estilo {
-	
-	@NotBlank(message="Nome é obrigatório")
+@Entity
+@Table(name = "estilo")
+public class Estilo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigo;
+
+	@OneToMany(mappedBy = "estilo")
+	private List<Cerveja> cervejas;
+
+	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
-	
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 
 	public String getNome() {
 		return nome;
@@ -14,5 +41,35 @@ public class Estilo {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Cerveja> getCervejas() {
+		return cervejas;
+	}
+
+	public void setCervejas(List<Cerveja> cervejas) {
+		this.cervejas = cervejas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (codigo ^ (codigo >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estilo other = (Estilo) obj;
+		if (codigo != other.codigo)
+			return false;
+		return true;
 	}
 }
