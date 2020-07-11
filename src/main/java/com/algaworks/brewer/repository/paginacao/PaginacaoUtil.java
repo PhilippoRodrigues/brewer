8124060b4db93f4c5b4d73652d6760ae3c70b07f
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-07-11 19:28:14 -0300

    Configuração da máscara do campo Telefone em CadastroCliente.html

--- src/main/java/com/algaworks/brewer/repository/paginacao/PaginacaoUtil.java
+++ src/main/java/com/algaworks/brewer/repository/paginacao/PaginacaoUtil.java
@@ -8,7 +8,6 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Root;
 
-import org.hibernate.Criteria;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
@@ -46,21 +45,20 @@ public class PaginacaoUtil {
 		return manager.createQuery(query);
 	}
 
-	public void preparar(Criteria criteria, Pageable pageable) {
-		int paginaAtual = pageable.getPageNumber();
-		int totalRegistrosPorPagina = pageable.getPageSize();
-		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
-
-		criteria.setFirstResult(primeiroRegistro);
-		criteria.setMaxResults(totalRegistrosPorPagina);
-
-		Sort sort = pageable.getSort();
-		if (sort != null) {
-			Sort.Order order = sort.iterator().next();
-			String property = order.getProperty();
-			criteria.addOrder(order.isAscending() ? org.hibernate.criterion.Order.asc(property)
-					: org.hibernate.criterion.Order.desc(property));
-		}
-	}
-
+//	public void preparar(Criteria criteria, Pageable pageable) {
+//		int paginaAtual = pageable.getPageNumber();
+//		int totalRegistrosPorPagina = pageable.getPageSize();
+//		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
+//
+//		criteria.setFirstResult(primeiroRegistro);
+//		criteria.setMaxResults(totalRegistrosPorPagina);
+//
+//		Sort sort = pageable.getSort();
+//		if (sort != null) {
+//			Sort.Order order = sort.iterator().next();
+//			String property = order.getProperty();
+//			criteria.addOrder(order.isAscending() ? org.hibernate.criterion.Order.asc(property)
+//					: org.hibernate.criterion.Order.desc(property));
+//		}
+//	}
 }
--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -12,13 +12,13 @@ Brewer.MaskMoney = (function() {
 	MaskMoney.prototype.enable = function() {
 
 		this.decimal.maskMoney({
-			decimal : ',',
-			thousands : '.'
+			decimal: ',',
+			thousands: '.'
 		});
 
 		this.plain.maskMoney({
-			precision : 0,
-			thousands : '.'
+			precision: 0,
+			thousands: '.'
 		});
 	}
 
@@ -26,8 +26,33 @@ Brewer.MaskMoney = (function() {
 
 }());
 
+Brewer.MaskPhoneNumber = (function() {
+	function MaskPhoneNumber() {
+		this.inputPhoneNumber = $('.js-phone-number');
+	}
+
+	MaskPhoneNumber.prototype.enable = function() {
+
+		var maskBehavior = function(val) {
+			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+		},
+			options = {
+				onKeyPress: function(val, e, field, options) {
+					field.mask(maskBehavior.apply({}, arguments), options);
+				}
+			};
+
+		this.inputPhoneNumber.mask(maskBehavior, options);
+
+	}
+	return MaskPhoneNumber;
+}());
+
 $(function() {
 
 	var maskMoney = new Brewer.MaskMoney();
 	maskMoney.enable();
+
+	var maskPhoneNumber = new Brewer.MaskPhoneNumber();
+	maskPhoneNumber.enable();
 });
