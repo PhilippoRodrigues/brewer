ea6a98e87bcc6abc73157c7a1d03ad618c1bff4d
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-07-12 15:46:27 -0300

    Inserção do conteúdo da tabela estado no select "estado"

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -1,14 +1,19 @@
 package com.algaworks.brewer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.brewer.model.TipoPessoa;
+import com.algaworks.brewer.repository.Estados;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClientesController {
+	
+	@Autowired
+	private Estados estados;
 	
 	//Quando acessar a URL /clientes/novo, vai retornar a página HTML cliente/CadastroCliente
 	
@@ -16,6 +21,8 @@ public class ClientesController {
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());
+		//Percorre a interface Estados e busca a lista de estados
+		mv.addObject("estados", estados.findAll());
 		return mv;
 	}
 	
