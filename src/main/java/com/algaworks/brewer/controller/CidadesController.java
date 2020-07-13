f46544a38b12a9f91a71afcebbb4ce6284b6685d
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-07-12 22:34:37 -0300

    Requisição Ajax para busca de cidades quando selecionado estado

--- src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -1,35 +1,51 @@
 package com.algaworks.brewer.controller;
 
-import javax.validation.Valid;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.algaworks.brewer.model.Cidade;
+import com.algaworks.brewer.repository.Cidades;
 
 @Controller
+@RequestMapping("/cidades")
 public class CidadesController {
+	
+	@Autowired
+	private Cidades cidades;
 	
 	//Quando acessar a URL /cervejas/novo, vai retornar a página cerveja/CadastroCerveja
 	
-	@RequestMapping("/cidades/novo")
+	@RequestMapping("/novo")
 	public String novo(Cidade cidade) {
 		return "cidade/CadastroCidade";
 	}
 	
-	@RequestMapping(value = "/cidades/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cidade cidade, BindingResult result, Model model, RedirectAttributes attributes) {
-		
-		if(result.hasErrors()) {
-			return novo(cidade);
+	@RequestMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(
+			@RequestParam(name="estado", defaultValue="-1") Long codigoEstado){
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		
-		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso!");
-		System.out.println("Cidade: " + cidade.getNome());
-		return "redirect:/cidades/novo";
+		return cidades.findByEstadoCodigo(codigoEstado);
 	}
 }
+	
+//	@RequestMapping(value = "/cidades/novo", method = RequestMethod.POST)
+//	public String cadastrar(@Valid Cidade cidade, BindingResult result, Model model, RedirectAttributes attributes) {
+//		
+//		if(result.hasErrors()) {
+//			return novo(cidade);
+//		}
+//		
+//		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso!");
+//		System.out.println("Cidade: " + cidade.getNome());
+//		return "redirect:/cidades/novo";
+//	}
