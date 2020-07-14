942637240f4e13559020c3d08695265133014cc0
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-07-14 12:38:07 -0300

    Salvar CPF/CNPJ sem formatação no BD

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.algaworks.brewer.model.Cliente;
 import com.algaworks.brewer.model.TipoPessoa;
 import com.algaworks.brewer.repository.Estados;
+import com.algaworks.brewer.service.CadastroClienteService;
 
 @Controller
 @RequestMapping("/clientes")
@@ -20,6 +21,9 @@ public class ClientesController {
 	
 	@Autowired
 	private Estados estados;
+	
+	@Autowired
+	CadastroClienteService cadastroClienteService;
 	
 	//Quando acessar a URL /clientes/novo, vai retornar a página HTML cliente/CadastroCliente
 	
@@ -37,6 +41,7 @@ public class ClientesController {
 		if(result.hasErrors()) {
 			return novo(cliente);
 		}
+		cadastroClienteService.salvar(cliente);
 		attributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
 		return new ModelAndView("redirect:/clientes/novo");
 	}
