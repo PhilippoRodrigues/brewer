c7c6cdc09cd9bd2ff6b6ba9f63b71cbabd7b2ebc
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-07-29 00:00:43 -0300

    Salvar senha criptografada com BCrypt

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -15,42 +15,48 @@ import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.Grupos;
 import com.algaworks.brewer.service.CadastroUsuarioService;
 import com.algaworks.brewer.service.exception.EmailJaCadastradoException;
+import com.algaworks.brewer.service.exception.SenhaObrigatoriaUsuarioException;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuariosController {
-	
+
 	@Autowired
 	CadastroUsuarioService cadastroUsuarioService;
-	
+
 	@Autowired
 	Grupos grupos;
-	
-	//Quando acessar a URL /clientes/novo, vai retornar a página HTML cliente/CadastroCliente
-	
+
+	// Quando acessar a URL /clientes/novo, vai retornar a página HTML
+	// cliente/CadastroCliente
+
 	@RequestMapping("/novo")
 	public ModelAndView novo(Usuario usuario) {
 		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
 		mv.addObject("grupos", grupos.findAll());
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "/novo", method = RequestMethod.POST)
-	public ModelAndView cadastrar(@Valid Usuario usuario, BindingResult result, Model model, RedirectAttributes attributes) {
-		
-		if(result.hasErrors()) {
+	public ModelAndView cadastrar(@Valid Usuario usuario, BindingResult result, Model model,
+			RedirectAttributes attributes) {
+
+		if (result.hasErrors()) {
 			return novo(usuario);
 		}
-		
+
 		try {
 			cadastroUsuarioService.salvar(usuario);
-			}catch(EmailJaCadastradoException e){
-				result.rejectValue("email", e.getMessage(), e.getMessage());
-				return novo(usuario);
-			}
-		
+		} catch (EmailJaCadastradoException e) {
+			result.rejectValue("email", e.getMessage(), e.getMessage());
+			return novo(usuario);
+		} catch (SenhaObrigatoriaUsuarioException e) {
+			result.rejectValue("senha", e.getMessage(), e.getMessage());
+			return novo(usuario);
+		}
+
 		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso!");
-		
+
 		return new ModelAndView("redirect:/usuarios/novo");
 	}
 }
