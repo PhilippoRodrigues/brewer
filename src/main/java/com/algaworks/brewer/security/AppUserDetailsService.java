3a89b87a8534704e407b3fb2adef0720ca595c0f
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-07-30 23:39:41 -0300

    Implementação das permissões do usuário

--- src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
+++ src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -1,9 +1,14 @@
 package com.algaworks.brewer.security;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,14 +20,22 @@ import com.algaworks.brewer.repository.Usuarios;
 
 @Service
 public class AppUserDetailsService implements UserDetailsService {
-	
+
 	@Autowired
 	private Usuarios usuarios;
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = usuarios.porEmailEAtivo(email);
-		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-		return new User(usuario.getEmail(), usuario.getSenha(), new HashSet<>());
+		Usuario usuario = usuarioOptional
+				.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		return new User(usuario.getEmail(), usuario.getSenha(), getPermissoes(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		List<String> permissoes = usuarios.permissoes(usuario);
+		permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.toUpperCase())));
+		return authorities;
 	}
 }
