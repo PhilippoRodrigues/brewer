8fa66ad2285dc866357b0a5937ac521b09d30be6
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-10-06 00:07:42 -0300

    Implementação de pesquisa de cliente na página CadastroVenda

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -1,16 +1,23 @@
 package com.algaworks.brewer.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -22,6 +29,7 @@ import com.algaworks.brewer.repository.Estados;
 import com.algaworks.brewer.repository.filter.ClienteFilter;
 import com.algaworks.brewer.service.CadastroClienteService;
 import com.algaworks.brewer.service.exception.CpfCnpjClienteJaCadastradoException;
+
 
 @Controller
 @RequestMapping("/clientes")
@@ -42,20 +50,19 @@ public class ClientesController {
 	public ModelAndView novo(Cliente cliente) {
 		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());
-		//Percorre a interface Estados e busca a lista de estados
 		mv.addObject("estados", estados.findAll());
 		return mv;
 	}
 	
 	@PostMapping("/novo")
 	public ModelAndView salvar(@Valid Cliente cliente, BindingResult result, RedirectAttributes attributes) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return novo(cliente);
 		}
 		
 		try {
-		cadastroClienteService.salvar(cliente);
-		}catch(CpfCnpjClienteJaCadastradoException e){
+			cadastroClienteService.salvar(cliente);
+		} catch (CpfCnpjClienteJaCadastradoException e) {
 			result.rejectValue("cpfOuCnpj", e.getMessage(), e.getMessage());
 			return novo(cliente);
 		}
@@ -66,7 +73,7 @@ public class ClientesController {
 	
 	@GetMapping
 	public ModelAndView pesquisar(ClienteFilter clienteFilter, BindingResult result,
-			@PageableDefault(size=2) Pageable pageable, HttpServletRequest httpServletRequest) {
+			@PageableDefault(size=3) Pageable pageable, HttpServletRequest httpServletRequest) {
 		
 		ModelAndView mv = new ModelAndView("cliente/PesquisaClientes");
 		
@@ -75,5 +82,22 @@ public class ClientesController {
 		PageWrapper<Cliente> paginaWrapper = new PageWrapper<>(clientes.filtrar(clienteFilter, pageable), httpServletRequest);
 		mv.addObject("pagina", paginaWrapper);
 		return mv;
+	}
+	
+	@RequestMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody List<Cliente> pesquisar(String nome){
+		validarTamanhoNome(nome);
+		return clientes.findByNomeStartingWithIgnoreCase(nome);
+	}
+
+	private void validarTamanhoNome(String nome) {
+		if(StringUtils.isEmpty(nome) || nome.length() < 3) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<Void> tratarIllegalArgumentException(IllegalArgumentException e){
+		return ResponseEntity.badRequest().build();
 	}
 }
