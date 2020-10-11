918443a9787363420c0dc72b430a0ab7348d16c7
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2020-10-11 20:50:14 -0300

    Models ItemVenda e TabelaItensVenda e teste unitário (JUnit)

---
+++ src/main/java/com/algaworks/brewer/venda/TabelaItensVenda.java
@@ -0,0 +1,25 @@
+package com.algaworks.brewer.venda;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.algaworks.brewer.model.ItemVenda;
+
+public class TabelaItensVenda {
+	
+	private List<ItemVenda> itens = new ArrayList<>();
+	
+	public BigDecimal getValorTotal() {
+		return itens
+				//Vai iterar todos os itens adicionados na tabela
+				.stream()
+				//Vai pegar o resultado da multiplicação de cada item que passou pelo método getValorTotal() 
+				.map(ItemVenda::getValorTotal)
+				//Vai reduzir para um único valor utilizando determinada técnica, que, nesse caso, é a SOMA 
+				//Assim, se houver vários itens, seus valores serão somados e apresentará apenas um valor, que é a soma deles.
+				.reduce(BigDecimal::add)
+				//Caso não haja nenhum item adicionado, retornada ZERO
+				.orElse(BigDecimal.ZERO);
+	}
+}
