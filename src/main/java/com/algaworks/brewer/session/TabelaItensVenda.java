8de527712f64e8078d3c88f90692977fbf566a13
Philippo <philippo.rodrigues@al.infnet.edu.br>
2020-11-04 22:09:48 -0300

    Atualização da quantidade adicionada de um mesmo item na tabela

--- src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
+++ src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
@@ -3,6 +3,7 @@ package com.algaworks.brewer.session;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
@@ -17,9 +18,8 @@ public class TabelaItensVenda {
 	private List<ItemVenda> itens = new ArrayList<>();
 	
 	public BigDecimal getValorTotal() {
-		return itens
-				//Vai iterar todos os itens adicionados na tabela
-				.stream()
+		//Vai iterar todos os itens adicionados na tabela
+		return itens.stream()
 				//Vai pegar o resultado da multiplicação de cada item que passou pelo método getValorTotal() 
 				.map(ItemVenda::getValorTotal)
 				//Vai reduzir para um único valor utilizando determinada técnica, que, nesse caso, é a SOMA 
@@ -30,19 +30,28 @@ public class TabelaItensVenda {
 	}
 	
 	public void adicionarItem(Cerveja cerveja, Integer quantidade) {
-		ItemVenda itemVenda = new ItemVenda();
-		itemVenda.setCerveja(cerveja);
-		itemVenda.setQuantidade(quantidade);
-		itemVenda.setValorUnitario(cerveja.getValor());
+		Optional<ItemVenda> itemVendaOptional = itens.stream()
+			.filter(i -> i.getCerveja().equals(cerveja))
+			.findAny();
 		
-		itens.add(itemVenda);
+		ItemVenda itemVenda = null;
+		if (itemVendaOptional.isPresent()) {
+			itemVenda = itemVendaOptional.get();
+			itemVenda.setQuantidade(itemVenda.getQuantidade() + quantidade);
+		} else {
+			itemVenda = new ItemVenda();
+			itemVenda.setCerveja(cerveja);
+			itemVenda.setQuantidade(quantidade);
+			itemVenda.setValorUnitario(cerveja.getValor());
+			itens.add(0, itemVenda);
+		}
 	}
 	
 	public int total() {
 		return itens.size();
 	}
 
-	public Object getItens() {
+	public List<ItemVenda> getItens() {
 		return itens;
 	}
 }
