95a737e9c411d2403c67597b177d6bf72228b413
Philippo <philippo.rodrigues@mobicare.com.br>
2021-02-28 15:38:01 -0300

    Ajustes2

--- src/main/java/com/algaworks/brewer/config/S3Config.java
+++ src/main/java/com/algaworks/brewer/config/S3Config.java
@@ -30,7 +30,8 @@ public class S3Config {
                 Objects.requireNonNull(
                         environment.getProperty("AWSAccessKeyId")),
                 Objects.requireNonNull(environment.getProperty("AWSSecretKey")));
-        AmazonS3 amazonS3 = new AmazonS3Client(credenciais, new ClientConfiguration());
+        @SuppressWarnings("deprecation")
+		AmazonS3 amazonS3 = new AmazonS3Client(credenciais, new ClientConfiguration());
         Region regiao = Region.getRegion(Regions.US_EAST_1);
         amazonS3.setRegion(regiao);
         return amazonS3;
